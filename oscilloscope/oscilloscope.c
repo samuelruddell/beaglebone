979dafd975570af9ac98d56783b2c153d25cd18e
Samuel Ruddell <samuelruddell@gmail.com>
2015-09-19 04:35:28 +0000

    major revisions to oscilloscope.c

--- oscilloscope/oscilloscope.c
+++ oscilloscope/oscilloscope.c
@@ -27,6 +27,7 @@ unsigned int port = 3306;
 static char *unix_socket = NULL;
 unsigned int flag = 0;
 
+
 /* IRQ handler thread */
 void *pruevtout0_thread(void *arg) {
 	do {
@@ -37,66 +38,59 @@ void *pruevtout0_thread(void *arg) {
 
 int main (int argc, char **argv)
 {
-	static void *pru1DataMemory;
-	static unsigned int *pru1DataMemory_int;
-
 	MYSQL *conn; 
 	MYSQL_RES *result;
 	MYSQL_ROW row;
-	char mysqlStr[70000];
-	int i, j;
-	unsigned int data[2048];
+
+	unsigned int runScope;
 
 	/* connect to database */
 	conn = mysqlConnect();
 
-	/* infinite loop */
+	/* initialise PRU */
+	static void *pru1DataMemory;
+	static unsigned int *pru1DataMemory_int;
+
+	char mysqlStr[60000];
+	unsigned int data[2048];
+	int i;
+
+	/* Initialize structure used by prussdrv_pruintc_intc */
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+	/* Allocate and initialize memory */
+	prussdrv_init ();
+	prussdrv_open (PRU_EVTOUT_0);
+
+	/* Map PRU's INTC */
+	prussdrv_pruintc_init(&pruss_intc_initdata);
+
+	/* Map PRU data ram memory */
+	prussdrv_map_prumem(PRUSS0_PRU1_DATARAM, &pru1DataMemory);
+	pru1DataMemory_int = (unsigned int *) pru1DataMemory;
+
+	/* Load and execute binary on PRU */
+	prussdrv_exec_program (PRU_1, "./pru.bin");
+
+	// query database for the value of runScope, wait until value = 1
 	while(1) {
-		// query database for the value of RUN, wait until value = 1
-		while(1) {
-			sleep(1);			// wait 1 second
-			mysql_query(conn, "SELECT value FROM parameters WHERE name = 'RUN' LIMIT 1");
-			result = mysql_store_result(conn);
-			row = mysql_fetch_row(result);
-			// check whether RUN == "1"
-			if(!strncmp(row[0], "1", 1)){
-				mysql_free_result(result);
-				// reset RUN value to 0 in MySQL database
-				if (mysql_query(conn, "REPLACE INTO parameters (name, value) VALUES ('RUN', 0);")) {
-					mysqlError(conn);
-				}
-				// break loop and begin oscilloscope
-				break;
-			}
-			mysql_free_result(result);
+		mysql_query(conn, "SELECT value FROM parameters WHERE name = 'RUN' LIMIT 1");
+		result = mysql_store_result(conn);
+		row = mysql_fetch_row(result);
+
+		// check whether runScope == "1"
+		if(!strncmp(row[0], "1", 1)){
+			runScope = 1;
+		} else {
+			runScope = 0;
 		}
+		mysql_free_result(result);
 
-		/* Initialize structure used by prussdrv_pruintc_intc   */
-		/* PRUSS_INTC_INITDATA is found in pruss_intc_mapping.h */
-		tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-
-		/* Allocate and initialize memory */
-		prussdrv_init ();
-		prussdrv_open (PRU_EVTOUT_0);
-
-		/* Map PRU's INTC */
-		prussdrv_pruintc_init(&pruss_intc_initdata);
-
-		/* Map PRU data ram memory */
-		prussdrv_map_prumem(PRUSS0_PRU1_DATARAM, &pru1DataMemory);
-		pru1DataMemory_int = (unsigned int *) pru1DataMemory;
-
-		/* Load and execute binary on PRU */
-		prussdrv_exec_program (PRU_1, "./pru.bin");
-
-		j = 0;
-		/* Wait for event completion from PRU */
-		prussdrv_pru_send_event(1);
-		prussdrv_pru_wait_event (PRU_EVTOUT_0);
-
-		// 50 steps to terminate loop correctly for now
-		while(j<50){
-			prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU1_ARM_INTERRUPT);
+		/* run oscilloscope */
+		while(runScope){
+			/* interrupt PRU */
+			// event 17 maps to r31.t31, event 18 maps to r31.t30
+			prussdrv_pru_send_wait_clear_event  ( 18, PRU_EVTOUT_0, 18);
 
 			/* Read PRU memory and store in MySQL database */
 			char *mysqlStrPointer = mysqlStr;
@@ -104,7 +98,6 @@ int main (int argc, char **argv)
 			mysqlStrPointer += sprintf(mysqlStrPointer, "REPLACE INTO data (i, time, adc) VALUES");
 
 			/* Build string for inserting data */
-			// note: does not account for pru writes overtaking reads 
 			for(i=0; i<2048; i++){
 				data[i] = *(pru1DataMemory_int + i);
 				// time[i] = data[i] >> 12;		// unpack time data
@@ -114,24 +107,18 @@ int main (int argc, char **argv)
 				} else {
 					mysqlStrPointer += sprintf(mysqlStrPointer, "('%hu', '%u', '%hu');", i, data[i] >> 12, data[i] & 0x0fff);
 				}	
-
 			}
 
 			/* Insert data to table */
 			if (mysql_query(conn, mysqlStr)) {
 				mysqlError(conn);
 			}
-
-			/* Interrupt PRU, wait for PRU_EVTOUT_0 */
-			prussdrv_pru_send_event (1);
-			prussdrv_pru_wait_event (PRU_EVTOUT_0);
-			j++;
 		}
-		/* Disable PRU and close memory mappings */
-		prussdrv_pru_disable(PRU_1);
-		prussdrv_exit ();
-
 	}
+
+	/* Disable PRU and close memory mappings */
+	prussdrv_pru_disable(PRU_1);
+	prussdrv_exit ();
 
 	/* disconnect from database */
 	mysqlDisconnect(conn);
