c12dd202fe1dda9c92b6b8c0d89fa70d33baaf6c
Samuel Ruddell <samuelruddell@gmail.com>
2015-10-17 09:50:25 +0000

    initial implementation of app.get params

--- node/routes/params.js
+++ node/routes/params.js
@@ -4,10 +4,13 @@
 // Routes for working with experiment parameters. Pass in the express app instance.
 //
 module.exports = function (app) {
-	
+	var database = require('../database')
+
 	// Ask the server for the current set of parameters.
 	app.get("/params/", function (req, res) {
-		// TODO:
+		database.query('SELECT ?? FROM ??', [['name','value'],'parameters'], function(data) {
+			res.send(data)
+		})
 	});
 
 	// Ask the server to update the current set of params.
