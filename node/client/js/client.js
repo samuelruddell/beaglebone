46c8ea5d619ed5937393d07ca8797d9cd27d61ac
Samuel Ruddell <samuelruddell@gmail.com>
2015-10-25 07:23:59 +0000

    implemented POST parameters from client

--- node/client/js/client.js
+++ node/client/js/client.js
@@ -1,6 +1,19 @@
 /* client side javascript functions */
 var socket = io();
 
+// on document ready
+$(document).ready(function() {
+  // fill all fields from database
+  getParams();
+
+  // listen for field changes and POST
+  $('#pidControl :input').change(function () {
+    var param = JSON.stringify({name: $(this).attr('id'), value: $(this).val()})
+    // POST data, GET parameters as callback	
+    $.post("/params/", param, getParams) 
+  })
+})
+
 // Plot data with flot
 socket.on('data', function (data) {
   $.plot("#oscilloscope", [data.data], {
@@ -20,7 +33,8 @@ function getParams() {
   $.get("/params/", function(data) {
     for (var obj in data) {
       try {
-        var elem = document.getElementById(data[obj].name).value = data[obj].value;
+        // insert values into form
+        document.getElementById(data[obj].name).value = data[obj].value;
       } catch (err) {
         // getElementById(...) is null
       }
