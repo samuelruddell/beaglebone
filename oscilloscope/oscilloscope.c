d1785e84dcd8e7da348cb851d57f890ab3cb16d3
Samuel Ruddell <sam.ruddell@auckland.ac.nz>
2016-09-13 01:18:37 +1200

    move gpio toggling to C program

--- oscilloscope/oscilloscope.c
+++ oscilloscope/oscilloscope.c
@@ -8,12 +8,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <mysql/mysql.h>
 #include "oscilloscope.h"
 
+#include <fcntl.h>
+#include <sys/mman.h>
+
 #define PRU_0	   	0
 #define PRU_1      	1
+
+/* gpio memory addresses */
+#define GPIO0 		0x44e07000
+#define GPIO1 		0x4804c000
+#define GPIO2 		0x481ac000
+#define GPIO3 		0x481ae000
+#define GPIO_SIZE	0xfff
+#define GPIO_CLR	0x190
+#define GPIO_SET	0x194
 
 /* mysql global variables */
 static char *host = "localhost";
@@ -25,11 +36,21 @@ unsigned int port = 3306;
 static char *unix_socket = NULL;
 unsigned int flag = 0;
 
+
 int main (int argc, char **argv)
 {
-	MYSQL *conn; 
+	MYSQL *conn;
 
 	unsigned int runScope = 1;
+
+	/* memory map gpio addresses */
+	struct GpioAddr gpio;
+
+	int fd = open("/dev/mem", O_RDWR);
+	gpio.gpio0_addr = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO0);
+	gpio.gpio1_addr = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1);
+	gpio.gpio2_addr = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO2);
+	gpio.gpio3_addr = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO3);
 
 	/* connect to database */
 	conn = mysqlConnect();
@@ -67,7 +88,7 @@ int main (int argc, char **argv)
 	pruSharedDataMemory_int = (unsigned int *) pruSharedDataMemory;
 
 	/* Load default parameters and store in PRU memory */
-	runScope = mysqlGetParameters(conn, pruSharedDataMemory_int);
+	runScope = mysqlGetParameters(conn, pruSharedDataMemory_int, gpio);
 
 	/* Load and execute binary on PRU */
 	prussdrv_exec_program (PRU_0, "./pru0.bin");
@@ -94,7 +115,7 @@ int main (int argc, char **argv)
 					mysqlStrPointer += sprintf(mysqlStrPointer, "(%hu,%u,%hu,%hu),", i, time[i] - time[0], data[i] >> 16, data[i] & 0x0fff);
 				} else {
 					mysqlStrPointer += sprintf(mysqlStrPointer, "(%hu,%u,%hu,%hu)", i, time[i] - time[0], data[i] >> 16, data[i] & 0x0fff);
-				}	
+				}
 			}
 
 			mysqlStrPointer += sprintf(mysqlStrPointer, " ON DUPLICATE KEY UPDATE i=VALUES(i), time=VALUES(time), dac=VALUES(dac), adc=VALUES(adc)");
@@ -110,13 +131,16 @@ int main (int argc, char **argv)
 		sleep(1);
 
 		/* Load settings and write to PRU memory */
-		runScope = mysqlGetParameters(conn, pruSharedDataMemory_int);
+		runScope = mysqlGetParameters(conn, pruSharedDataMemory_int, gpio);
 	}
 
 	/* Disable PRU and close memory mappings */
 	prussdrv_pru_disable(PRU_0);
 	prussdrv_pru_disable(PRU_1);
 	prussdrv_exit ();
+
+	/* close mmap file */
+	close(fd);
 
 	/* disconnect from database */
 	mysqlDisconnect(conn);
@@ -139,7 +163,7 @@ MYSQL * mysqlConnect(){
 }
 
 /* Load default settings and write to PRU memory */
-unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_int){
+unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_int, struct GpioAddr gpio){
 	MYSQL_RES *result;
 	MYSQL_ROW row;
 
@@ -152,6 +176,8 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
 	unsigned int scanPoint = 1;	// ensure scanPoint and openAmpl are initialised
 	unsigned int openAmpl = 1;
         unsigned int span;
+	static unsigned int muxOut, muxIn;	// static variables to store previous value of multiplexer
+
 	//unsigned int ireset_pos_neg     = 0x0;
 
 	mysql_query(conn, "SELECT addr, value FROM parameters");
@@ -195,7 +221,7 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
 				break;
 
 			// pack XLOCK and YLOCK
-			case 2 :	
+			case 2 :
 				xlock_ylock |= ((mem_value & 0xffff) << 16);    // XLOCK
 				break;
 			case 3 :
@@ -206,11 +232,11 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
                         case 5 :
                                 // this case is reserved for OSC_STEP
                                 break;
-			case 13 :	
+			case 13 :
 				open_point_ampl |= (mem_value & 0xffff);        // OPENAMPL
                                 openAmpl = (mem_value & 0xffff);
 				break;
-			case 14 : 			
+			case 14 :
 				open_point_ampl |= ((mem_value & 0xffff) << 16);// SCANPOINT
                                 scanPoint = (mem_value & 0xffff);
 				break;
@@ -221,14 +247,14 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
 				break;
 
 			// pack AUTO INTEGRATOR OVERFLOW and UNDERFLOW
-			case 21 : 	
+			case 21 :
 				//ireset_pos_neg |= ((mem_value & 0xffff) << 16); // POSITIVE OVERFLOW
 				break;
 			case 22 :
 				//ireset_pos_neg |= (mem_value & 0xffff);         // NEGATIVE UNDERFLOW
 				break;
 
-			// do nothing (parameter not relevant to PRU program)
+			// parameter not relevant to PRU program
 			case 100 :
 			case 101 :
 			case 102 :
@@ -239,18 +265,32 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
 			case 107 :
 			case 108 :
 				break;
+			case 110 :
+				// output multiplexer value
+				if (mem_value!=muxOut){
+					muxOut = mem_value;
+					setMuxOut(gpio, muxOut);
+				}
+				break;
+			case 111 :
+				// input multiplexer value
+				if (mem_value!=muxIn){
+					muxIn = mem_value;
+					setMuxIn(gpio, muxIn);
+				}
+				break;
 
-			// write memory normally
-			default : 			
+			// else write memory normally
+			default :
 				*(pruSharedDataMemory_int + mem_offset) = mem_value;
 		}
 	}
 
 	// store packed data to pru memory
-	*(pruSharedDataMemory_int + 1) = pruBooleans;           
-	*(pruSharedDataMemory_int + 2) = xlock_ylock;          
-	*(pruSharedDataMemory_int + 13) = open_point_ampl;    
-	//*(pruSharedDataMemory_int + 21) = ireset_pos_neg;    
+	*(pruSharedDataMemory_int + 1) = pruBooleans;
+	*(pruSharedDataMemory_int + 2) = xlock_ylock;
+	*(pruSharedDataMemory_int + 13) = open_point_ampl;
+	//*(pruSharedDataMemory_int + 21) = ireset_pos_neg;
 
         // calculate open loop oscilloscope step, and send to PRU
         if(openAmpl>scanPoint){
@@ -268,7 +308,7 @@ unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_i
         *(pruSharedDataMemory_int + 5) = span/2047;
 
 	// clean up and return runScope
-	mysql_free_result(result);	
+	mysql_free_result(result);
 	runScope = runScope & bOpenClose;
 	return runScope;
 }
@@ -283,4 +323,84 @@ void mysqlError(MYSQL *conn){
 	fprintf(stderr, "%s\n", mysql_error(conn));
 	mysql_close(conn);
 	exit(1);
+}
+
+int setMuxOut(struct GpioAddr gpio, int value) {
+	#ifdef INVERT_MUXOUT_GPIO
+		// output multiplexer expects pin high = off
+		int set = GPIO_CLR;
+		int clr = GPIO_SET;
+	#else
+		// output multiplexer expects pin low = off
+		int set = GPIO_SET;
+		int clr = GPIO_CLR;
+	#endif
+
+	// P8_7		gpio 66 (gpio2_2) 0x090
+	// P8_9 	gpio 69 (gpio2_5) 0x09c
+
+	gpio.ClearAddr = gpio.gpio2_addr + clr;
+	*gpio.ClearAddr = (1 << 2) | (1 << 5);		// clear both
+	switch (value) {
+		case 1 :
+			// P8_7 on
+			gpio.SetAddr = gpio.gpio2_addr + set;
+			*gpio.SetAddr = 1 << 2;
+			break;
+		case 2 :
+			// P8_9 on
+			gpio.SetAddr = gpio.gpio2_addr + set;
+			*gpio.SetAddr = 1 << 5;
+			break;
+		default :
+			// else both outputs off
+			break;
+	}
+
+	return 0;
+}
+
+int setMuxIn(struct GpioAddr gpio, int value) {
+
+	//P9_24	gpio 15 (gpio0_15), 0x184
+	//P9_26	gpio 14 (gpio0_14), 0x180
+	//P9_25	gpio 117 (gpio3_21), 0x1ac
+	//P9_23	gpio 49 (gpio1_17), 0x044
+
+	/* clear all gpio first */
+	gpio.ClearAddr = gpio.gpio0_addr + GPIO_CLR;
+	*gpio.ClearAddr = 0b11 << 14;
+	gpio.ClearAddr = gpio.gpio1_addr + GPIO_CLR;
+	*gpio.ClearAddr = 1 << 17;
+	gpio.ClearAddr = gpio.gpio3_addr + GPIO_CLR;
+	*gpio.ClearAddr = 1 << 21;
+
+	/* set gpio based on value */
+	switch (value) {
+		case 1 :
+			// P9_24 on
+			gpio.SetAddr = gpio.gpio0_addr + GPIO_SET;
+			*gpio.SetAddr = 1 << 15;
+			break;
+		case 2 :
+			// P9_26 on
+			gpio.SetAddr = gpio.gpio0_addr + GPIO_SET;
+			*gpio.SetAddr = 1 << 14;
+			break;
+		case 3 :
+			// P9_25 on
+			gpio.SetAddr = gpio.gpio3_addr + GPIO_SET;
+			*gpio.SetAddr = 1 << 21;
+			break;
+		case 4 :
+			// P9_23 on
+			gpio.SetAddr = gpio.gpio1_addr + GPIO_SET;
+			*gpio.SetAddr = 1 << 17;
+			break;
+		default :
+			// else all off
+			break;
+	}
+
+	return 0;
 }
--- oscilloscope/oscilloscope.h
+++ oscilloscope/oscilloscope.h
@@ -1,4 +1,13 @@
+#define INVERT_MUXOUT_GPIO
+
+struct GpioAddr {
+	volatile void *gpio0_addr, *gpio1_addr, *gpio2_addr, *gpio3_addr;
+	volatile unsigned int *SetAddr, *ClearAddr;
+};
+
 MYSQL * mysqlConnect();
-unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_int);
+unsigned int mysqlGetParameters(MYSQL *conn, unsigned int *pruSharedDataMemory_int, struct GpioAddr);
 void mysqlDisconnect(MYSQL *conn);
 void mysqlError(MYSQL *conn);
+int setMuxOut(struct GpioAddr, int );
+int setMuxIn(struct GpioAddr, int );
